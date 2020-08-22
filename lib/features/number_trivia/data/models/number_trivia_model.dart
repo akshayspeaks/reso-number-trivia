b5862287854f32af40e64e22bf89bfdaad94e514
Akshay Shukla <akshay@pop-os.localdomain>
2020-08-22 19:17:28 +0530

    Lecture 4

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:meta/meta.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({@required String text, @required int number})
+      : super(number: number, text: text);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+        text: json['text'], number: (json['number'] as num).toInt());
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'text': text, 'number': number};
+  }
+}
